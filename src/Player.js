34982296285ea28379e5570a079b3638f04d21a5
stephanieessis@gmail.com <stephanieessis@gmail.com>
2025-03-11 12:32:32 +0000

    fix:players

---
+++ src/Player.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const Player = ({ name, team, nationality, jerseyNumber, age, imageUrl }) => {
+  return (
+    <Card style={{ width: "18rem", margin: "10px" }}>
+      <Card.Img variant="top" src={imageUrl} alt={name} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          <strong>Équipe :</strong> {team} <br />
+          <strong>Nationalité :</strong> {nationality} <br />
+          <strong>Numéro :</strong> {jerseyNumber} <br />
+          <strong>Âge :</strong> {age}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+// Définir des valeurs par défaut pour les props
+Player.defaultProps = {
+  name: "Inconnu",
+  team: "Sans équipe",
+  nationality: "Non spécifié",
+  jerseyNumber: 0,
+  age: 0,
+  imageUrl: "https://via.placeholder.com/150",
+};
+
+export default Player;
---
+++ src/Players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Simon Adingra",
+      team: "Brightton",
+      nationality: "ivoirien",
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: "../assets/adin.jpeg",
+    },
+    {
+      name: "Camarvinga",
+      team: "Real de Madrid",
+      nationality: "francais",
+      jerseyNumber: 7,
+      age: 39,
+      imageUrl: "../assets/cama.jpeg ",
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "Français",
+      jerseyNumber: 7,
+      age: 25,
+      imageUrl: "../assets/mbappe.jpeg",
+    },
+    {
+      name: "Neymar Jr",
+      team: "Al-Hilal",
+      nationality: "Brésilien",
+      jerseyNumber: 10,
+      age: 32,
+      imageUrl: "../assets/neymar.jpeg",
+    }
+  ];
+  
+  export default players;
+  
